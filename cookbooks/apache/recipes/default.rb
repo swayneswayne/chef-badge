2eac51e4182e1ebd31e5c2b6f61ccea27d7c892e
swayne <swayneswayne@gmail.com>
2017-03-14 21:06:39 +0000

    added apache changed

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,13 +5,21 @@
 #
 # # Copyright:: 2017, The Authors, All Rights Reserved.
 #
+
+if node['platform_familly'] == "rhel"
+	package = "http"
+elsif node['platform_family'] == "debian"
+	package = "apache2"
+end
+
 package 'apache2' do
-	package_name 'httpd'
-	action install
+	package_name package
+	action :install
 end
 
 service 'apache2'do
 	service_name 'httpd'
 	action [:start, :enable]
 end
+
 
